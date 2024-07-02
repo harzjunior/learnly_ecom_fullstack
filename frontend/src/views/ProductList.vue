52b7753a1bf7d3df397ea82c95575a92d14092b6
harzjunior <harunjibs@gmail.com>
2024-07-02 10:09:24 +0100

    styling on search field

--- frontend/src/views/ProductList.vue
+++ frontend/src/views/ProductList.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="flex flex-col items-center justify-center min-h-screen bg-gray-200 mt-1 mb-10 pt-10">
     <div class="w-full max-w-5xl">
-      <h1 class="text-4xl text-center font-bold mb-10 text-gray-800">Product List</h1>
-      <h2 class="text-2xl text-center font-bold mb-10 text-gray-600">Welcome, {{ username }}</h2>
 
       <!-- Search input field -->
-      <div class="mb-6">
+      <div class="flex items-center justify-between mb-10">
+        <h1 class="text-4xl text-center font-bold text-gray-800 px-20">Product List</h1>
         <input v-model="searchTerm" type="text" placeholder="Search products..."
-          class="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+          class="w-1/2 px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
       </div>
 
+      <h2 class="text-2xl text-center font-bold mb-10 text-gray-600">Welcome, {{ username }}</h2>
       <div v-if="loading" class="text-center">
         <p>Loading products...</p>
       </div>
